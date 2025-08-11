f3543b7a98210bf10bf083321025ca287b00983c
brtran97 <brtran97@gmail.com>
2025-08-11 03:58:04 -0700

    changed calculator to return target TDEE

--- app/src/main/java/GragasApp/model/MifflinStJeorCalculator.java
+++ app/src/main/java/GragasApp/model/MifflinStJeorCalculator.java
@@ -3,8 +3,16 @@ package GragasApp.model;
 public class MifflinStJeorCalculator implements CalorieCalculator{
 
   @Override
-  // Uses formula from MifflinStJeor to calculate BMR and TDEE
+  // Uses formula from MifflinStJeor to calculate BMR and TDEE for target wieght
   public double calculateBmr(UserProfile user) {
+    // Units: weight kg, height cm
+    double base = 10.0 * user.getTargetWeightKg() + 6.25 * user.getHeightCm() - 5.0 * user.getAge();
+    // if male +5 if female -161
+    double sexOffset = (user.getSex() == Sex.MALE) ? 5.0 : -161.0;
+    return base + sexOffset;
+  }
+
+  public double calculateCurrentBmr(UserProfile user) {
     // Units: weight kg, height cm
     double base = 10.0 * user.getWeightKg() + 6.25 * user.getHeightCm() - 5.0 * user.getAge();
     // if male +5 if female -161
