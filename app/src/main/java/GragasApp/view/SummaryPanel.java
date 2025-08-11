cf2651f5cc42616d33c59fdb0709a2d8b1a60737
brtran97 <brtran97@gmail.com>
2025-08-11 00:24:57 -0700

    added some controller interfaces and add the calculator Panel

---
+++ app/src/main/java/GragasApp/view/SummaryPanel.java
@@ -0,0 +1,28 @@
+package GragasApp.view;
+
+import GragasApp.model.*;
+import GragasApp.controller.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SummaryPanel extends JPanel {
+  private final JLabel totalCaloriesToday = new JLabel("-");
+  private final JLabel poundsToGoal = new JLabel("-");
+
+  public SummaryPanel() {
+    setLayout(new GridLayout(2, 2, 8, 8));
+    add(new JLabel("Total calories today:"));
+    add(totalCaloriesToday);
+    add(new JLabel("Pounds to goal:"));
+    add(poundsToGoal);
+  }
+
+  // Call these from controller after model updates TO-DO
+  public void setTotalCaloriesToday(int cals) {
+    totalCaloriesToday.setText(String.valueOf(cals));
+  }
+  public void setPoundsToGoal(double lbs) {
+    poundsToGoal.setText(String.format("%.1f", lbs));
+  }
+}
